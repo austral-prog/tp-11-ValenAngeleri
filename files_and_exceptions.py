e5abfbb4ca3556602e193fcc4facbcf7eaf4e376
ValenAngeleri <vangeleri@mail.austral.edu.ar>
2025-06-18 14:01:13 -0300

    Update files_and_exceptions.py

--- files_and_exceptions.py
+++ files_and_exceptions.py
@@ -1,17 +1,21 @@
 def read_file_to_dict(filename):
-    """Lee un archivo de ventas donde cada venta es producto:valor_de_venta;... y agrupa los valores por producto en una lista.
+    ventas = {}
+    with open(filename, "r") as file:
+        line = file.readline().strip()
+        items = line.split(";")
+        for item in items:
+            if not item:
+                continue
+            producto , valor = item.split(":")
+            valor = float(valor)
 
-    :param filename: str - nombre del archivo a leer.
-    :return: dict - diccionario con listas de montos por producto.
-    :raises: FileNotFoundError - si el archivo no existe.
-    """
-    return {}
-
+            if producto not in ventas:
+                ventas[producto]=[]
+            ventas[producto].append(valor)
+        return ventas
 
 def process_dict(data):
-    """Para cada producto, imprime el total de ventas y el promedio, en el orden natural del diccionario.
-
-    :param data: dict - diccionario a procesar.
-    :return: None
-    """
-    pass
+    for producto, valores in data.items():
+        total = (sum(valores))
+        promedio = (total/len(valores))
+        print(f"{producto}: ventas totales ${total}0, promedio ${promedio}0")
